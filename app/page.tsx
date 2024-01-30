13fb1770d18929c1e6a7b22180b5eabb852915db
moelzanaty3 <mohammedelzanaty129@gmail.com>
2024-01-30 20:47:42 +0200

    chore: add Clerk authentication middleware and components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import { SignInButton, SignOutButton, auth } from '@clerk/nextjs';
 
 export default function Home() {
+  const { userId } : { userId: string | null } = auth();
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -39,6 +42,13 @@ export default function Home() {
         />
       </div>
 
+      {!userId && (<SignInButton />)}
+      {userId && (
+            <>
+              <p className="text-2xl font-semibold">Welcome, {userId}</p>
+              <SignOutButton />
+            </>
+      )}
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
